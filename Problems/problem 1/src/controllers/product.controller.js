534e5b2e25d951f3491fc76809f5bd81a4daa62b
Nitish Kumar <121974807+devlpr-nitish@users.noreply.github.com>
2024-06-26 12:29:06 +0530

    Delete Problems directory

--- Problems/problem 1/src/controllers/product.controller.js
+++
@@ -1,7 +0,0 @@
-// Please don't change the pre-written code
-// Import the necessary modules here
-import path from 'path';
-
-export const getProducts = (req, res) => {
-  return res.sendFile(path.resolve('src', 'views', 'index.html'));
-};
--- Problems/problem 7/server.js
+++
@@ -1,5 +0,0 @@
-import server from "./index.js";
-
-server.listen(3000, () => {
-  console.log("server is listening at port 3000");
-});
